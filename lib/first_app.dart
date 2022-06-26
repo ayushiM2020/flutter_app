83851dde9c670800b082ca0306dcb7e29cd9d256
ayushiM2020 <89742259+ayushiM2020@users.noreply.github.com>
2022-06-26 01:00:29 -0600

    files uploaded

---
+++ lib/first_app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({Key? key}) : super(key: key);
+  _LearnFlutterPageState createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Learn Flutter'),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/sunflower.jpg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+            height: 50,
+          )
+        ],
+      ),
+    );
+  }
+}
